cf29688783cd06308a7df3fef460a466342abf1e
Hiroyuki Ito <ZXB01226@nifty.com>
2022-03-30 23:33:57 +0900

    * ngraph-gtk.rb: fix typo.

--- ngraph-gtk.rb
+++ ngraph-gtk.rb
@@ -22,9 +22,9 @@ class NgraphGtk < Formula
     system "make"
     system "(cd po; make ja.gmo)"
     system "make", "install"
-    p formula.prefix
-    p formula.prefix.directory?
-    p Keg.new(formula.prefix).empty_installation?
+    p self.prefix
+    p self.prefix.directory?
+    p Keg.new(self.prefix).empty_installation?
   end
 
   test do
