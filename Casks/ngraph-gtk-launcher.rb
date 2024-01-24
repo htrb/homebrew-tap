5e243aa966a42457d6571a9938a1da45ec66f966
Hiroyuki Ito <ZXB01226@nifty.com>
2024-01-24 11:39:14 +0900

    * Casks/ngraph-gtk-launcher.rb: download dmg file.

--- Casks/ngraph-gtk-launcher.rb
+++ Casks/ngraph-gtk-launcher.rb
@@ -1,21 +1,11 @@
 cask "ngraph-gtk-launcher" do
   version "1.0.0"
-  sha256 "c21ef066bf2a53e6a482828e00e9ca0ed8c576c20e9920bf82263d82da4a8c3d"
+  sha256 "346ade8fba9e64e8edcf3d78f856a90e0d8a85b11d3a6b3adb96819dae9125cc"
 
-  url "https://github.com/htrb/ngraph-gtk-launcher/archive/refs/tags/v#{version}.zip"
+  url "https://github.com/htrb/ngraph-gtk-launcher/releases/download/v#{version}/ngraph-gtk.dmg"
   name "Ngraph-GTK Launcher"
   desc "Launcher App for ngraph-gtk installed with homebrew."
   homepage "https://github.com/htrb/ngraph-gtk-launcher"
-
-  livecheck do
-    url "https://github.com/htrb/ngraph-gtk-launcher/releases/tag/v#{version}"
-    strategy :page_match do |page|
-      match = page.match(/v(\d).(\d+).(\d+)\.zip/i)
-      next if match.blank?
-
-      "#{match[1]}.#{match[2]}.#{match[3]}"
-    end
-  end
 
   depends_on macos: ">= :sierra"
 
