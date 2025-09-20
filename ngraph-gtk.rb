c2ee70d06ca8c2d94b9db92863b09e45237cf2ab
Hiroyuki Ito <ZXB01226@nifty.com>
2025-09-20 15:16:09 +0900

    new upstream relate (6.09.10).

--- ngraph-gtk.rb
+++ ngraph-gtk.rb
@@ -1,24 +1,13 @@
 class NgraphGtk < Formula
-  option "with-gtk4", "Use GTK4 instead of GTK3."
-
-  NGRAPH_VERSION = "6.09.09".freeze
+  NGRAPH_VERSION = "6.09.10".freeze
   NGRAPH_REPOSITORY = "https://github.com/htrb/ngraph-gtk.git".freeze
   desc "Create scientific 2-dimensional plots"
   homepage "https://github.com/htrb/ngraph-gtk"
-  if build.with? "gtk4"
-    head NGRAPH_REPOSITORY, branch: "gtk4"
-    depends_on "gtk4"
-    depends_on "gtksourceview5"
-  else
-    url NGRAPH_REPOSITORY, tag: "v#{NGRAPH_VERSION}"
-    license "GPL-2.0-or-later"
-    head NGRAPH_REPOSITORY, branch: "master"
-    depends_on "gtk+3"
-    depends_on "gtksourceview4"
-    on_macos do
-      depends_on "gtk-mac-integration"
-    end
-  end
+  url NGRAPH_REPOSITORY, tag: "v#{NGRAPH_VERSION}"
+  license "GPL-2.0-or-later"
+  head NGRAPH_REPOSITORY, branch: "master"
+  depends_on "gtk4"
+  depends_on "gtksourceview5"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -31,7 +20,7 @@ class NgraphGtk < Formula
   def install
     ENV["PKG_CONFIG_PATH"] += ":#{ENV["HOMEBREW_PREFIX"]}/lib/pkgconfig"
     system "autoreconf", "-if"
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    system "./configure", *std_configure_args, "--disable-silent-rules", "--enable-nls"
     system "make"
     Dir.chdir("po") do
       system "make", "ja.gmo"
